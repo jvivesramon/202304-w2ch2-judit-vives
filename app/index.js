0a0f33870bc6ec29ae024b29a97ae8621343b067
Judit Vives <jvivesramon@gmail.com>
2023-04-12 08:53:21 +0200

    Add result console.log

--- app/index.js
+++ app/index.js
@@ -3,4 +3,11 @@ import { strictEquals } from "./boleanFunction/index.js";
 let valueA = 0;
 let valueB = -0;
 
-console.log(strictEquals(valueA, valueB));
+console.log(`1 === 1 --- ${strictEquals(1, 1)}`);
+console.log(`NaN === NaN --- ${strictEquals(NaN, NaN)}`);
+console.log(`0 === -0 --- ${strictEquals(0, -0)}`);
+console.log(`-0 === 0 --- ${strictEquals(-0, 0)}`);
+console.log(`1 === "1" --- ${strictEquals(1, "1")}`);
+console.log(`true === false --- ${strictEquals(true, false)}`);
+console.log(`false === false --- ${strictEquals(false, false)}`);
+console.log(`"fish" === "chips" --- ${strictEquals("fish", "chips")}`);
